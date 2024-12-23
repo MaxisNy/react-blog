14496a5dc65af180772943295f9e0fcd03f2f0ce
Maxim Nyanin <maximnyanin@gmail.com>
2024-12-23 17:43:32 -0500

    Implemented Front-End

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,14 +1,28 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import NavBar from './NavBar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
+    <BrowserRouter>
+      <div className="App">
+      <NavBar />
       <div id="page-body">
-        Welcome to My Blog!
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+          <Route path="*" element={<NotFoundPage />}/>
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   )
 }
 
