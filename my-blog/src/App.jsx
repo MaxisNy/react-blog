d08407fb3dc039d276fbfeb4cfe6baf3d273ca11
Maxim Nyanin <maximnyanin@gmail.com>
2024-12-23 13:57:15 -0500

    Created a project environment

---
+++ my-blog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>My Awesome Blog</h1>
+      <div id="page-body">
+        Welcome to My Blog!
+      </div>
+    </div>
+  )
+}
+
+export default App
