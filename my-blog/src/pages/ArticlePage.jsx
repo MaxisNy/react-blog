36b4e16dc85d110810605333746a30d946679c24
Maxim Nyanin <maximnyanin@gmail.com>
2024-12-28 11:53:59 -0500

    Connected frontend with backend

--- my-blog/src/pages/ArticlePage.jsx
+++ my-blog/src/pages/ArticlePage.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 import articles from './article-content';
 
 const ArticlePage = () => {
@@ -20,6 +22,12 @@ const ArticlePage = () => {
 
     const article = articles.find(article => article.name === articleId);
 
+    const addUpvote = async () => {
+        const reponse = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = reponse.data;
+        setArticleInfo(updatedArticle);
+    }
+
     if (!article) {
         return <NotFoundPage />
     }
@@ -27,10 +35,17 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
+        <AddCommentForm
+            articleName={articleId}
+            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        <CommentsList comments={articleInfo.comments}></CommentsList>
         </>
     );
 }
