f0529c40c720f5447aadb1288b993a25e885e31f
Maxim Nyanin <maximnyanin@gmail.com>
2024-12-27 23:10:26 -0500

    Implemented upvote data fetching and configured vite proxy

--- my-blog/src/pages/ArticlePage.jsx
+++ my-blog/src/pages/ArticlePage.jsx
@@ -1,9 +1,23 @@
-import { useParams } from "react-router-dom";
-import NotFoundPage from "./NotFoundPage";
-import articles from "./article-content";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import NotFoundPage from './NotFoundPage';
+import articles from './article-content';
 
 const ArticlePage = () => {
-    const {articleId} = useParams();
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
+    }, []);
+
     const article = articles.find(article => article.name === articleId);
 
     if (!article) {
@@ -12,10 +26,11 @@ const ArticlePage = () => {
 
     return (
         <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph, i) => (
-                <p key={i}>{paragraph}</p>
-            ))}
+        <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
         </>
     );
 }
