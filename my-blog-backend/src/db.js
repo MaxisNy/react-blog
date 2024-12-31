9db1a96d9cb201bcc9da071d80f9f3ae4dd76887
Maxim Nyanin <maximnyanin@gmail.com>
2024-12-31 18:32:41 -0500

    Security fixes

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@fullstackblog-cluster.ctv4a.mongodb.net/?retryWrites=true&w=majority&appName=FullStackBlog-Cluster`);
     await client.connect();
     db = client.db('react-blog-db'); // use react-blog-db
     cb();
